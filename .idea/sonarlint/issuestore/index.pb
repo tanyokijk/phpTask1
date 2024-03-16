cf18650c05329d03de066384117e1093cfe8a2c8
Тетяна Деревянко <t.derevyanko1209@gmail.com>
2024-03-16 19:11:32 +0200

    1

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+9
+	index.php,b\b\bb6499b8e938f92a3695fff1afe57edea4b9efb7
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+T
+$.idea/codeStyles/codeStyleConfig.xml,d\d\dd7b3350c29c49497756c7f32c69a3bed18b1cba
+8
+code.php,4\0\40233b45cd3ef318eddf06189c19eec223c64db5
+Å
+Q.idea/sonarlint/securityhotspotstore/b/b/bb6499b8e938f92a3695fff1afe57edea4b9efb7,3\3\33146ca102dfb9ad1815170db874f476c67ed65e
+\
+,.idea/inspectionProfiles/Project_Default.xml,4\9\496a238a6afa168dbaf6efd37bb459331589579c
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
---
+++ index.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+
+<?php
+$Name = "Tanya";
+echo "Hello! My name is '$Name'";
+?>
+<br>
+<?php
+$Age = 18;
+echo "I`m " . $Age;
+?>
+<br>
+<?php
+$a = 6;
+$b = 7;
+$rez = $a + $b;
+echo "'$a' + '$b' = '$rez'";
+?>
+<br>
+<?php
+echo "До перестановки: a = $a, b = $b\n";
+
+$a = $a + $b;
+$b = $a - $b;
+$a = $a - $b;
+
+echo " Після: a = $a, b = $b\n";
+$questions = array(
+    'first' => ['A' => true, 'B' => false, 'C' => false, 'D' => false],
+    'second' => ['A' => true, 'B' => true, 'C' => false, 'D' => false],
+    'third' => ['A' => true, 'B' => true, 'C' => true, 'D' => false],
+    'fourth' => 'Answer',
+    'fifth' => 'Answer',
+    'sixth' => 'Answer'
+);
+?>
+
+<form>
+    <label><?= array_keys($questions)[0] ?> :</label>
+    <br>
+    <?php
+    foreach ($questions['first'] as $key => $value): ?>
+        <?php
+        if ($value === true): ?>
+            <input type="radio" value="<?= $key ?>">
+            <label for="<?= $key ?>" style="color: green;"><?= $key ?></label><br>
+        <?php
+        else: ?>
+            <input type="radio" value="<?= $key ?>">
+            <label for="<?= $key ?>"><?= $key ?></label><br>
+        <?php
+        endif; ?>
+    <?php
+    endforeach; ?>
+
+    <label><?= array_keys($questions)[1] ?> :</label>
+    <br>
+    <?php
+    foreach ($questions['second'] as $key => $value): ?>
+        <?php
+        if ($value === true): ?>
+            <input type="checkbox" value="<?= $key ?>">
+            <label for="<?= $key ?>" style="color: green;"><?= $key ?></label><br>
+        <?php
+        else: ?>
+            <input type="checkbox" value="<?= $key ?>">
+            <label for="<?= $key ?>"><?= $key ?></label><br>
+        <?php
+        endif; ?>
+    <?php
+    endforeach; ?>
+
+    <label><?= array_keys($questions)[2] ?> :</label>
+    <br>
+    <?php
+    foreach ($questions['third'] as $key => $value): ?>
+        <?php
+        if ($value === true): ?>
+            <input type="checkbox" value="<?= $key ?>">
+            <label for="<?= $key ?>" style="color: green;"><?= $key ?></label><br>
+        <?php
+        else: ?>
+            <input type="checkbox" value="<?= $key ?>">
+            <label for="<?= $key ?>"><?= $key ?></label><br>
+        <?php
+        endif; ?>
+    <?php
+    endforeach; ?>
+
+
+    <br>
+    <label><?= array_keys($questions)[3] ?> :</label>
+    <input type="text" placeholder="<?= $questions['fourth'] ?>">
+    <br>
+
+    <br>
+    <label><?= array_keys($questions)[4] ?> :</label>
+    <input type="text" placeholder="<?= $questions['fifth'] ?>">
+    <br>
+
+    <br>
+    <label><?= array_keys($questions)[5] ?> :</label>
+    <input type="text" placeholder="<?= $questions['sixth'] ?>">
+</form>
+<?php
+$tag = 'Hello';
+$background_color_blue = 'blue';
+$color_red = 'red';
+$width_100px = '100px';
+$height_100px = '100px';
+echo "<br>background_color : " . $background_color_blue;
+echo "<br>color: " . $color_red;
+echo "<br>width: " . $width_100px;
+echo "<br>height: " . $height_100px;
+?>
+<div style="
+        background-color: <?= $background_color_blue ?>;
+        color: <?= $color_red ?>;
+        width: <?= $width_100px ?>;
+        height: <?= $height_100px ?>;">
+
+    <?= $tag ?>
+</div>
+
+</body>
+</html>
